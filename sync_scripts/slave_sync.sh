630bb17d6f964bc5177b0976183032f7077b4ecf
root <root@pulp-slave-server.local>
2017-08-05 23:05:55 +0530

    Adding repo creation and repo deletion sync to slave_sync.sh

--- sync_scripts/slave_sync.sh
+++ sync_scripts/slave_sync.sh
@@ -6,7 +6,38 @@
 #=========================================================================================================
 
 
-echo -e "Now, syncing and publishing all repos on the slave \n";
+REPOS_FILE=/opt/scripts/all_repos.txt
+
+
+# <-- This is to make sure that if the session certificate has expired, it will be renewed
+
+pulp-admin repo list > /dev/null
+
+if [ $? == 77 ];
+then
+        pulp-admin login -u admin -p admin
+fi
+
+# Ends here--> 
+
+
+if [ $(diff /opt/scripts/all_repos.txt <(pulp-admin repo list | grep ^Id | cut -d ":" -f2) | wc -l) -ne 0 ]
+	then
+		if [ $(diff /opt/scripts/all_repos.txt <(pulp-admin repo list | grep ^Id | cut -d ":" -f2) | grep "<" | wc -l) -ne 0 ]
+			then
+			for i in `diff /opt/scripts/all_repos.txt <(pulp-admin repo list | grep ^Id | cut -d ":" -f2) | grep "<" | sed 's/<//'`
+			do
+			# Enter the IP / Name of the master server
+			pulp-admin rpm repo create --repo-id "$i" --feed http://pulpserver.local/pulp/repos/"$i"/ --relative-url "$i"/ --serve-http true
+			done
+		else
+			for i in `diff /opt/scripts/all_repos.txt <(pulp-admin repo list | grep ^Id | cut -d ":" -f2) | grep ">" | sed 's/>//'`
+			do
+			pulp-admin rpm repo delete --repo-id $i
+			done
+		fi
+fi
+
 
 for i in `pulp-admin repo list | grep -i id | cut -d ":" -f2`; do pulp-admin rpm repo sync run --repo-id "$i" ; pulp-admin rpm repo publish run --repo-id "$i"; done;
 
